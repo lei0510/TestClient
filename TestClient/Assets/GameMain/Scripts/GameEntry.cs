d1f9491e112c929d804aaa7878b5e26384aac1b4
leihongbin <479973947@qq.com>
2020-09-01 12:24:00 +0800

    初始化

---
+++ TestClient/Assets/GameMain/Scripts/GameEntry.cs
@@ -0,0 +1,15 @@
+﻿using GameFramework.Fsm;
+using GameFramework.Procedure;
+using UnityEngine;
+
+namespace TestClient
+{
+    public class GameEntry:ProcedureBase
+    {
+        protected override void OnEnter(IFsm<IProcedureManager> procedureOwner)
+        {
+            base.OnEnter(procedureOwner);
+            Debug.LogError("Entry");
+        }
+    }
+}
